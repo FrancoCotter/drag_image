d6473be00bc89642d874cd4d7d24a4f1c15ee0a4
FrancoCotter <chockyeizo@hotmail.com>
2015-08-05 20:52:40 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
-window.onload=function(){
-
+function drag(event){
+  event.dataTransfer.setData('Text',e.target.id);
+}
+function drop(event){
+  var data=event.dataTransfer.getDate('Text');
+  event.target.appendChild(document.getElementById(data));
+  event.preventDefault;
+}
+function allowDrag(event){
+  event.preventDefault;
 }
